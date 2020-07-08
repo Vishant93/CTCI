c010d6c9c72059dc1a852b1403a4348f35289e38
Vishant93 <vishantk@gmail.com>
2020-07-07 21:16:33 -0700

    progression

--- src/LinkedLists/DeleteMiddleNode.java
+++ src/LinkedLists/DeleteMiddleNode.java
@@ -1,5 +1,11 @@
 package LinkedLists;
 
 public class DeleteMiddleNode {
-
+    public static boolean deleteNode(LinkedListNode n) {
+        if (n == null || n.next == null) return null;
+        LinkedListNode next = n.next;
+        n.data = next.data;
+        n.next = next.next;
+        return true;
+    }
 }
